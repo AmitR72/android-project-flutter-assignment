1572747ca741e53948d719a21bbc3bd83b5498c8
Amit Rush <rushamit@cmapus.technion.ac.il>
2022-03-31 10:35:02 +0300

    first commit hw2. installations

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,33 @@
 import 'package:english_words/english_words.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+      if (snapshot.hasError) {
+        return Scaffold(
+            body: Center(
+                child: Text(snapshot.error.toString(),
+                    textDirection: TextDirection.ltr)));
+      }
+      if (snapshot.connectionState == ConnectionState.done) {
+        return MyApp();
+      }
+      return Center(child: CircularProgressIndicator());
+        },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
